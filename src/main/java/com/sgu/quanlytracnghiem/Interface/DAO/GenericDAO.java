4489ab4d2efb51f2e4911f31e25be9a37ef9495a
magiauy <68595741+magiauy@users.noreply.github.com>
2025-02-27 22:18:55 +0700

    Create

--- src/main/java/com/sgu/quanlytracnghiem/Interface/DAO/GenericDAO.java
+++ src/main/java/com/sgu/quanlytracnghiem/Interface/DAO/GenericDAO.java
@@ -1,9 +1,13 @@
 package com.sgu.quanlytracnghiem.Interface.DAO;
 
+import java.util.ArrayList;
+
 public interface GenericDAO <T> {
     void insert(T obj);
     void update(T obj);
     void delete(String id);
     T getById(String id);
+
+    ArrayList<T> getAll();
 
 }
